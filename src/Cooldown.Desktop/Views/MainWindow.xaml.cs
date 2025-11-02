413aa888d85f7284fe81d6dd05b9979aa8e4b814
Chris020206 <christofferjensen7@gmail.com>
2025-11-02 21:03:12 +0100

    Enable Windows Forms framework reference for desktop app

---
+++ src/Cooldown.Desktop/Views/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Windows;
+using Cooldown.Desktop.Services;
+using Cooldown.Desktop.ViewModels;
+
+namespace Cooldown.Desktop.Views;
+
+public partial class MainWindow : Window
+{
+    private readonly MainViewModel _viewModel;
+    private readonly ToastNotificationService _toastService;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        var configService = new BlockerConfigService();
+        var engineHost = new BlockerEngineHost();
+        _viewModel = new MainViewModel(configService, engineHost, Dispatcher);
+        DataContext = _viewModel;
+        _toastService = new ToastNotificationService();
+        _viewModel.ToastRequested += OnToastRequested;
+        Loaded += OnLoadedAsync;
+        Closed += OnClosedAsync;
+    }
+
+    private async void OnLoadedAsync(object sender, RoutedEventArgs e)
+    {
+        await _viewModel.InitializeAsync();
+    }
+
+    private async void OnClosedAsync(object? sender, EventArgs e)
+    {
+        _viewModel.ToastRequested -= OnToastRequested;
+        _toastService.Dispose();
+        await _viewModel.DisposeAsync();
+    }
+
+    private void OnToastRequested(object? sender, ToastNotificationEventArgs e)
+    {
+        _toastService.Show(e.Title, e.Message);
+    }
+}
